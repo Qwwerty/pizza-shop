3580e97f6683b552946e82eebd33c2a035beb357
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2024-03-17 16:01:56 -0300

    feat: first commit

---
+++ src/routes.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './page/_layouts/app'
+import { AuthLayout } from './page/_layouts/auth'
+import { Dashboard } from './page/app/dashboard'
+import { SignIn } from './page/auth/sign-in'
+import { SignUp } from './page/auth/sign-up'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
+  },
+])
