2b0adc791c60dbc64102991a70c45601ea678acb
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2024-03-24 12:25:08 -0300

    feat: not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './page/_layouts/app'
 import { AuthLayout } from './page/_layouts/auth'
+import { NotFound } from './page/404'
 import { Dashboard } from './page/app/dashboard/dashboard'
 import { Orders } from './page/app/orders/orders'
 import { SignIn } from './page/auth/sign-in'
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
